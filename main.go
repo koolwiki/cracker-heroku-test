3c0b2ef3c724990497b5d8667c006556b57bd2f6
koolwiki <hbxntsxw@hotmail.com>
2018-10-11 09:50:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,13 +12,14 @@ func main() {
 	var port, secret string
 	port = os.Getenv("PORT")
 	secret = os.Getenv("SECRET")
+	rpAddr = os.Getenv("RPADDR")
 	if port == "" {
 		port = "80"
 	}
 	if secret == "" {
 		secret = "123456"
 	}
-        if rpAddr == "" {
+	if rpAddr == "" {
 		rpAddr = "http://mirrors.xmission.com/ubuntu/"
 	}
 	addr := fmt.Sprintf(":%s", port)
