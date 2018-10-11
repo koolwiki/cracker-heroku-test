e89068648713dc2373a2ed3564636054e747238a
koolwiki <hbxntsxw@hotmail.com>
2018-10-11 09:00:51 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/koolwiki/cracker"
+)
+
+func main() {
+
+	var port, secret string
+	port = os.Getenv("PORT")
+	secret = os.Getenv("SECRET")
+	if port == "" {
+		port = "80"
+	}
+	if secret == "" {
+		secret = "123456"
+	}
+  if rpAddr == "" {
+		rpAddr = "http://mirrors.xmission.com/ubuntu/"
+	}
+	addr := fmt.Sprintf(":%s", port)
+	p := cracker.NewHttpProxy(addr, secret, false, rpAddr)
+	p.Listen()
+
+}
